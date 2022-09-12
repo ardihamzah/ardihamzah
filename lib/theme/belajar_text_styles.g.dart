00324c3cc7be9b4c7e170250df224e535b218524
Parabeac-Nest <admin@parabeac.com>
2022-09-12 05:29:00 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/belajar_text_styles.g.dart
@@ -0,0 +1,80 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class BelajarTextStyles {
+
+static const TextStyle bodyTextCTABtn = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -0.27,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle heroH1 = TextStyle(
+  fontSize: 56,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.84,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyPLarge = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.54,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subPageH1 = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.72,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyH4 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -0.36,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyP = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.27,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle bodyPSmall = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.21,
+  fontFamily: 'Montserrat',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
